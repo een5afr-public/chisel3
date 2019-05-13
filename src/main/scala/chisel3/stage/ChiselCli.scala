chipsalliance/chisel

Add chisel3.stage.ChiselCli
Signed-off-by: Schuyler Eldridge <schuyler.eldridge@ibm.com>

src/main/scala/chisel3/stage/ChiselCli.scala
@@ -1,0 +1,12 @@
+// See LICENSE for license details.
+
+package chisel3.stage
+
+import firrtl.options.Shell
+
+trait ChiselCli { this: Shell =>
+  parser.note("Chisel Front End Options")
+  Seq( NoRunFirrtlCompilerAnnotation,
+       PrintFullStackTraceAnnotation )
+    .map(_.addOptions(parser))
+}

een5afr-public/chisel3

Co-Authored-By: Schuyler Eldridge <schuyler.eldridge@ibm.com>
Co-Authored-By: chick <chick@qrhino.com>

src/main/scala/chisel3/stage/ChiselCli.scala
@@ -1,0 +1,12 @@
+// See LICENSE for license details.
+
+package chisel3.stage
+
+import firrtl.options.Shell
+
+trait ChiselCli { this: Shell =>
+  parser.note("Chisel Front End Options")
+  Seq( NoRunFirrtlCompilerAnnotation,
+       PrintFullStackTraceAnnotation )
+    .foreach(_.addOptions(parser))
+}